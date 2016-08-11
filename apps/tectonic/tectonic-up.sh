d6b79bed6ed87ffdbf91128a8222cc4f7ef03e57
Aleks Saul <aleks.saul@gmail.com>
2016-08-10 21:06:32 -0400

    bump tectonic-console to v0.2.0

--- apps/tectonic/tectonic-up.sh
+++ apps/tectonic/tectonic-up.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e 
-TECTONICNAMESPACE=tectonic-system
-kubectl create namespace $TECTONICNAMESPACE
 
+${TECTONICNAMESPACE:-tectonic-system}
+${TECTONIC_PULL_SECRET:-$HOME/Downloads/coreos-pull-secret.yaml}
+
+kubectl create namespace $TECTONICNAMESPACE
+kubectl --namespace=tectonic-system create -f "$TECTONIC_PULL_SECRET" 2> /dev/stdout 1> /dev/null
 kubectl --namespace=$TECTONICNAMESPACE create -f `dirname $0`/manifests/replicationcontroller.yaml
 kubectl --namespace=$TECTONICNAMESPACE create -f `dirname $0`/manifests/service.yaml
