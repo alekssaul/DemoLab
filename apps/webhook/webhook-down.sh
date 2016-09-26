fb427c1144fc4e2053785f6fdf13c6fdc184e2bc
Aleks Saul <aleks.saul@gmail.com>
2016-09-26 13:53:07 -0400

    changes to webhook to support on AWS

--- apps/webhook/webhook-down.sh
+++ apps/webhook/webhook-down.sh
@@ -5,7 +5,13 @@ echo `date` - Executing $0 ...
 
 WEBHOOKNAMESPACE=${WEBHOOKNAMESPACE:-webhook}
 
-kubectl --namespace=$WEBHOOKNAMESPACE delete svc webhook
-kubectl delete namespace $WEBHOOKNAMESPACE
+if [[ "$DemoLab_SETUP_TECTONIC_Enterprise" == "true" ]] ; then
+	kubeconfig=$DemoLab_RootFolder/infra/aws/cluster/kubeconfig
+else
+	kubeconfig=$HOME/.kube/config
+fi
+
+kubectl --kubeconfig=$kubeconfig --namespace=$WEBHOOKNAMESPACE delete svc webhook
+kubectl --kubeconfig=$kubeconfig delete namespace $WEBHOOKNAMESPACE
 
 echo `date` - Finished Executing $0  
--- apps/webhook/webhook-up.sh
+++ apps/webhook/webhook-up.sh
@@ -7,7 +7,13 @@ WEBHOOKNAMESPACE=${WEBHOOKNAMESPACE:-webhook}
 
 kubectl create namespace $WEBHOOKNAMESPACE
 
-kubectl --namespace=$WEBHOOKNAMESPACE create -f `dirname $0`/manifests/replicationcontroller.yaml
-kubectl --namespace=$WEBHOOKNAMESPACE create -f `dirname $0`/manifests/service.yaml
+if [[ "$DemoLab_SETUP_TECTONIC_Enterprise" == "true" ]] ; then
+	kubeconfig=$DemoLab_RootFolder/infra/aws/cluster/kubeconfig
+else
+	kubeconfig=$HOME/.kube/config
+fi
+
+kubectl --kubeconfig=$kubeconfig --namespace=$WEBHOOKNAMESPACE create -f `dirname $0`/manifests/replicationcontroller.yaml
+kubectl --kubeconfig=$kubeconfig --namespace=$WEBHOOKNAMESPACE create -f `dirname $0`/manifests/service.yaml
 
 echo `date` - Finished Executing $0 
