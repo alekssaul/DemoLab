0644aeff6ec5beba55be5458bf855c4d283a831f
Aleks Saul <aleks.saul@gmail.com>
2016-09-27 08:58:10 -0400

    WIP - QE and ELB for QE

---
+++ apps/quay/quay-up.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo `date` - Executing $0 ...
+QUAY_NAMESPACE=${QUAY_NAMESPACE:-quay-enterprise}
+
+if [[ "$DemoLab_SETUP_Quay" == "true" ]] ; then
+	kubeconfig=$DemoLab_RootFolder/infra/aws/cluster/kubeconfig
+else
+	kubeconfig=$HOME/.kube/config
+fi
+
+kubectl --kubeconfig=$kubeconfig  create -f $DemoLab_RootFolder/apps/quay/manifests/_quay-enterprise-namespace.yml
+kubectl --kubeconfig=$kubeconfig --namespace=$QUAY_NAMESPACE create -f $TECTONIC_PULL_SECRET
+kubectl --kubeconfig=$kubeconfig --namespace=$QUAY_NAMESPACE create -f  $DemoLab_RootFolder/apps/quay/manifests/quay-enterprise-config-secret.yml
+kubectl --kubeconfig=$kubeconfig --namespace=$QUAY_NAMESPACE create -f  $DemoLab_RootFolder/apps/quay/manifests/quay-enterprise-redis.yml
+kubectl --kubeconfig=$kubeconfig --namespace=$QUAY_NAMESPACE create -f  $DemoLab_RootFolder/apps/quay/manifests/quay-enterprise-service-nodeport.yml
+kubectl --kubeconfig=$kubeconfig --namespace=$QUAY_NAMESPACE create -f  $DemoLab_RootFolder/apps/quay/manifests/quay-enterprise-app-rc.yml
+
+echo `date` - Finished Executing $0 
